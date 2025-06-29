62074fa61232c45e379897aa41612abcc5696322
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-06-29 19:44:36 -0300

    Adiciona questão 003

---
+++ 003/index.py
@@ -0,0 +1,13 @@
+def media_escolar():
+  nota1 = 7.5
+  nota2 = 8.0
+  nota3 = 6.5
+  
+  media = (nota1 + nota2 + nota3) / 3
+  
+  print(f"Nota 1: {nota1}")
+  print(f"Nota 2: {nota2}")
+  print(f"Nota 3: {nota3}")
+  print(f"Média final: {media:.2f}")
+
+media_escolar()
