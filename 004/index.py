e4373a71a081992302d85d30fbc0cfed3a5e5e96
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2025-06-29 19:44:40 -0300

    Adiciona questão 004

---
+++ 004/index.py
@@ -0,0 +1,11 @@
+def consumo_combustivel():
+  distancia = 300
+  combustivel = 25
+  
+  consumo_medio = distancia / combustivel
+  
+  print(f"Distância percorrida: {distancia} km")
+  print(f"Combustível gasto: {combustivel} litros")
+  print(f"Consumo médio: {consumo_medio:.2f} km/l")
+
+consumo_combustivel()
